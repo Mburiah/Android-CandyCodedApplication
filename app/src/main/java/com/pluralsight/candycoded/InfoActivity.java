b4ab1a057a6cc46b8f37b14922a330bc0478ae42
Mburiah <bykieswagix5@gmail.com>
2020-10-06 00:28:54 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -33,9 +33,8 @@ public class InfoActivity extends AppCompatActivity {
         Intent mapIntent = new Intent(Intent.ACTION_VIEW);
         mapIntent.setPackage("com.google.android.apps.maps");
 
-        if(){
-            mapIntent.resolveActivity(getPackageManager());
-            return null;
+        if (mapIntent.resolveActivity(getPackageManager()) !== null){
+            startActivity(mapIntent);
         }
     }
     // ***
